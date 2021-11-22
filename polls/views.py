2477df273e28db577aea78d1656145e303bb395e
Thalita França <ferreira.conceicao@escolar.ifrn.edu.br>
2021-11-22 11:40:30 -0300

    parte3/2

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Olá mundo, essa é uma resposta HTTP')
+
+def detail(request, question_id):
+    return HttpResponse("Questão %s. " % question_id)
+
+def detail(request, question_id):
+    response = "Resposta %s. "
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voto %s. " % question_id)
